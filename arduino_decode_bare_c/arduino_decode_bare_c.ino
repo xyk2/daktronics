273ccad85427547e513f4f9213fa52faad96ee3a
Xiaoyang Kao <kaoxiaoyang@gmail.com>
2013-01-06 02:27:49 +0800

    Added a 'Enable splits' switch

--- arduino_decode_bare_c/arduino_decode_bare_c.ino
+++ arduino_decode_bare_c/arduino_decode_bare_c.ino
@@ -8,13 +8,28 @@ void setup() {
   Serial.begin(19200);
   inputString.reserve(BUFFER_STRING_SIZE);
   oldString.reserve(BUFFER_STRING_SIZE);
-  pinMode(9, INPUT);
+    Serial.println("Serial Ports Registers");
+  Serial.println("----------------------");
+  
+  Serial.print("UCSR0A: ");
+  Serial.println(UCSR0A,BIN);
+  Serial.print("UCSR0B: ");
+  Serial.println(UCSR0B,BIN);
+  Serial.print("UCSR0C: ");
+  Serial.println(UCSR0C,BIN);
+  Serial.print("UBRR0H: ");
+  Serial.println(UBRR0H,BIN);
+  Serial.print("UBRR0L: ");
+  Serial.println(UBRR0L,BIN);
 }
 
 char rxbuffer[BUFFER_STRING_SIZE];
 
 void loop() {
-  while (Serial.available()) {
+      Serial.println(!(UCSR0A & (1<<RXC0)));
+      Serial.println(Serial.available());
+  delay(100);
+  /*while (Serial.available()) {
     if(Serial.read() == 2) {
       if(Serial.readBytesUntil((char)4, rxbuffer, BUFFER_STRING_SIZE)) { // if successful
         for(int x = 0; x < BUFFER_STRING_SIZE; x++) { // add chars to string
@@ -30,7 +45,7 @@ void loop() {
             Serial.print("t"); // t for time
             Serial.println(inputString);
           }
-          else if(digitalRead(9) == 0 && oldString != inputString) { // if split is different from last split transmission
+          else if(oldString != inputString) { // if split is different from last split transmission
             Serial.print("s"); // s for split
             Serial.println(inputString.substring(0,4) + ' ' + inputString.substring(4));
             for(int x = 0; x < 4; x++) {
@@ -51,7 +66,7 @@ void loop() {
         memset(rxbuffer,0, BUFFER_STRING_SIZE); 
       }
     }
-  }
+  }*/
 }
 
 
