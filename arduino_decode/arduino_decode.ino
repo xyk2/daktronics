d787006276c974fed6a29ba20f4908700dbefa28
Xiaoyang Kao <kaoxiaoyang@gmail.com>
2013-01-06 11:16:31 +0800

    Added status led and direct port access; general code cleanup

--- arduino_decode/arduino_decode.ino
+++ arduino_decode/arduino_decode.ino
@@ -1,59 +1,77 @@
 #define BUFFER_STRING_SIZE 50
 String inputString = "";         // a string to hold incoming data
 String oldString = "";
-String winning_time = "";
-byte TOTAL_LAPS = 10;
+char rxbuffer[BUFFER_STRING_SIZE];
 
 void setup() {
   Serial.begin(19200);
+  DDRB |= (1<<PORTB0); // status LED output
+  DDRB |= (0<<PORTB1); // split switch input  
+  if((PINB & (1<<PORTB1))) {
+    flash_status_led(4, 80);
+    RUN_TEST_MODE();
+    while(1) {}; // enter infinite loop
+  }
   inputString.reserve(BUFFER_STRING_SIZE);
   oldString.reserve(BUFFER_STRING_SIZE);
-  pinMode(9, INPUT);
 }
 
-char rxbuffer[BUFFER_STRING_SIZE];
-
 void loop() {
-  while (Serial.available()) {
-    if(Serial.read() == 2) {
-      if(Serial.readBytesUntil((char)4, rxbuffer, BUFFER_STRING_SIZE)) { // if successful
-        for(int x = 0; x < BUFFER_STRING_SIZE; x++) { // add chars to string
-          if((int)rxbuffer[x] != 0) inputString += rxbuffer[x]; // skip null values; add to string
-          //if(rxbuffer[x] == ' ') inputString += ' '; // add another space for >10:00.0 times
-        }
-        inputString.trim(); // remove leading and trailing whitespace
-        
-        char *p = rxbuffer;
-
-        if(inputString.indexOf(".") != -1) { // if string does not include time, skip
-          if(inputString.length() <= 7) {
-            Serial.print("t"); // t for time
-            Serial.println(inputString);
+  while(1) {
+    while (Serial.available()) {
+      if(Serial.read() == 2) {
+        if(Serial.readBytesUntil((char)4, rxbuffer, BUFFER_STRING_SIZE)) { // if successful
+          for(int x = 0; x < BUFFER_STRING_SIZE; x++) { // add chars to string
+            if((int)rxbuffer[x] != 0) inputString += rxbuffer[x]; // skip null values; add to string
           }
-          else if(digitalRead(9) == 0 && oldString != inputString) { // if split is different from last split transmission
-            Serial.print("s"); // s for split
-            Serial.println(inputString.substring(0,4) + ' ' + inputString.substring(4));
-            for(int x = 0; x < 4; x++) {
-              String str = strtok_r(p, " ", &p); // delimiter is the semicolon
-              if(x == 3) { // laps
-                if(str == String(TOTAL_LAPS)) Serial.println('w' + winning_time); //if matches finish race
-              }
-              if(x == 2) winning_time = str;
+          inputString.trim(); // remove leading and trailing whitespace
+          
+          char *p = rxbuffer;
+  
+          if(inputString.indexOf(".") != -1) { // if string does not include time, skip
+            if(inputString.length() <= 7) {
+              Serial.print("t"); // t for time
+              Serial.println(inputString);
             }
-            oldString = inputString;
+            else if((PINB & (1<<PORTB1)) == 0 && oldString != inputString) { // if split is different from last split transmission
+              PORTB = (1<<PORTB0);
+              Serial.print("s"); // s for split
+              Serial.println(inputString.substring(0,4) + ' ' + inputString.substring(4));
+              oldString = inputString;
+              PORTB = (0<<PORTB0);
+            }
           }
-          
-          
+          inputString = "";
+          memset(rxbuffer,0, BUFFER_STRING_SIZE); 
         }
-        
-        
-        inputString = "";
-        memset(rxbuffer,0, BUFFER_STRING_SIZE); 
       }
     }
   }
 }
 
+
+void RUN_TEST_MODE(void) {
+  for(int x=0; x<5000; x++) {
+    Serial.print("t");
+    Serial.println(x*0.1, 1);
+    if(x == 33) Serial.println("s5 1 3.36 2"); 
+    if(x == 45) Serial.println("s3 2 4.52 2"); 
+    if(x == 65) Serial.println("s7 3 6.50 2"); 
+    if(x == 150) Serial.println("s3 1 15.05 4"); 
+    if(x == 165) Serial.println("s5 2 16.54 4"); 
+    if(x == 166) Serial.println("s7 3 16.60 4");        
+    delay(100);
+  }
+}
+
+void flash_status_led(byte number_of_flashes, unsigned int milliseconds) {
+  for(byte x = 0; x<number_of_flashes; x++) {
+    PORTB = (1<<PORTB0);
+    delay(milliseconds);
+    PORTB = (0<<PORTB0);
+    delay(milliseconds); 
+  }
+}
 
 
 
